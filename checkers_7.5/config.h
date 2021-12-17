e2fac441efc21285ae5e8b8e7b97c26d63dd1b02
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-12-17 22:28:02 +0300

    Update config.h

--- checkers_7.5/config.h
+++ checkers_7.5/config.h
@@ -23,3 +23,75 @@ const char* host = "http://checkers021.000webhostapp.com/";
 #define brightnessLevel 90
 #define colour1         ledMatrix.Color(255, 255, 255)
 #define colour2         ledMatrix.Color(255,0,0)
+
+#define CA1 0
+#define CB1 0
+#define CC1 1
+#define CD1 0
+#define CE1 2
+#define CF1 0
+#define CG1 3
+#define CH1 0
+
+#define CA2 0
+#define CB2 7
+#define CC2 0
+#define CD2 6
+#define CE2 0
+#define CF2 5
+#define CG2 0
+#define CH2 4
+
+#define CA3 8
+#define CB3 0
+#define CC3 9
+#define CD3 0
+#define CE3 10
+#define CF3 0
+#define CG3 11
+#define CH3 0
+
+#define CA4 0
+#define CB4 15
+#define CC4 0
+#define CD4 14
+#define CE4 0
+#define CF4 13
+#define CG4 0
+#define CH4 12
+
+#define CA5 16
+#define CB5 0
+#define CC5 17
+#define CD5 0
+#define CE5 18
+#define CF5 0
+#define CG5 19
+#define CH5 0
+
+#define CA6 0
+#define CB6 23
+#define CC6 0
+#define CD6 22
+#define CE6 0
+#define CF6 21
+#define CG6 0
+#define CH6 20
+
+#define CA7 24
+#define CB7 0
+#define CC7 25
+#define CD7 0
+#define CE7 26
+#define CF7 0
+#define CG7 27
+#define CH7 0
+
+#define CA8 0
+#define CB8 31
+#define CC8 0
+#define CD8 30
+#define CE8 0
+#define CF8 29
+#define CG8 0
+#define CH8 28
