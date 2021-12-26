7c40568b17cb63cf18401a3a0b7175a0061b6aaa
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-12-26 22:53:35 +0300

    Update main.py

--- Python-server/checkers/main.py
+++ Python-server/checkers/main.py
@@ -1,2 +1,24 @@
-def app(event):
-    return "Hello, world!!!!"
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+
+def respond():
+    data = request.json
+    command = data.get('request', {}).get('command', '')
+
+    if command:
+        response_text = 'Ненавижу Python!'
+    else:
+        response_text = 'Привет'
+
+    response = {
+        'response': {
+            'text': response_text,
+        },
+        'version': '1.0'
+    }
+    return response
+
