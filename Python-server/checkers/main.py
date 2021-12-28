926a43e6fd0c9a00369579b9dc4c5e79a806f7fe
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-12-28 03:21:38 +0300

    Add files via upload

--- Python-server/checkers/main.py
+++ Python-server/checkers/main.py
@@ -1,18 +1,16 @@
 import uvicorn
-import asyncio
 from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 @app.post("/")
-async def read_root(request = Body(..., embed=True)):
+def read_root(request = Body(..., embed=True)):
     results = {
         "response": {
-            "text":request['command']
+            "text": request['command']
             },
             "version": "1.0"
         }
-    return results 
-
+    return results
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8010, reload=True)
